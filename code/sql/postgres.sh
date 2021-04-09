3916a62fa53ba859ef992c7b38745523723635aa
dereck-rasmussen <43160546+dereck-rasmussen@users.noreply.github.com>
2021-04-09 15:02:41 -0700

    Update postgres.sh

--- code/sql/postgres.sh
+++ code/sql/postgres.sh
@@ -9,3 +9,4 @@ describe <table>;
 \dt *.* # List tables from all schemas (if *.* is omitted will only show SEARCH_PATH ones)
 \du # Show users
 create database <database>; # create new db
+\x auto # pretty print
