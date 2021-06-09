6cab783eaa3915543dda51f6e8cfed73fd167ebe
Dereck Rasmussen <drasmussen@vmware.com>
2021-06-09 10:17:03 -0700

    Fix add-alias.sh for zsh

--- code/command_line/add-alias.sh
+++ code/command_line/add-alias.sh
@@ -1,20 +1,21 @@
 # Add alias to dotfile:
 rx.add_alias() {
-  local aliases_file=${HOME}/.bash_aliases
+  local dotfile_path="${HOME}/.zshrc"
+  local aliases_file=${dotfile_path}
   local timestamp=$(date "+%Y-%m-%d")
-  local most_recent=$(history 2)
+  local most_recent=$(history)
   echo '=== ADD ALIAS ==='
   echo 'Your most recent command was:'
   echo
-  echo "$most_recent"
+  echo "${most_recent}"
   echo
   echo 'Enter the command to alias'
-  read -p 'Command: ' command
+  read command
   echo 'Enter a name for the alias (rx prefix?)'
-  read -p 'Alias: ' alias_name
+  read alias_name
   echo 'Enter note: (optional)'
-  read -p 'Note: ' alias_note
+  read alias_note
   echo "alias ${alias_name}=\"${command}\" # ${alias_note} ${timestamp}" >> ${aliases_file}
-  source ${HOME}/.bash_aliases
-  echo "SUCCESS: 'rx.${alias_name}' has been added to your '${aliases_file}' and is ready to use."
+  source ${dotfile_path}
+  echo "SUCCESS: '${alias_name}' has been added to your '${aliases_file}' and is ready to use."
 }
